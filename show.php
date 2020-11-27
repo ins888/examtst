024d5bbe9cff9d700e7b4f901caa1a02ce8200f6
ins888 <73679447+ins888@users.noreply.github.com>
2020-11-27 10:30:36 +0700

    Update show.php

--- show.php
+++ show.php
@@ -11,7 +11,7 @@
     <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070110_test', '3654Pp', 'exam', 3306);
+mysqli_real_connect($conn, 'ttt.mysql.database.azure.com', 'ins888@ttt', '022943654Pp', 'exam', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
